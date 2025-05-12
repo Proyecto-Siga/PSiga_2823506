70a95bef18bee9ea823a930d2cd03f642cf91c9d
Edwin1538 <179547472+Edwin1538@users.noreply.github.com>
2025-05-12 08:40:17 -0500

    CHANGE Migracion Asignatura

--- CODIGO PROYECTO/BACKEND/database/migrations/2025_05_12_131516_create_asignaturas_table.php
+++ CODIGO PROYECTO/BACKEND/database/migrations/2025_05_12_131516_create_asignaturas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('asignaturas', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre_aula');
+            $table->string('nombre_asignatura');
             $table->timestamps();
         });
     }
