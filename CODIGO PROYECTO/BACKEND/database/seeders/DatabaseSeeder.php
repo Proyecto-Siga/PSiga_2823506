aa41512ca6d1482f1d8ec905243595b314fc961b
EstibenCG <183879300+EstibenCG@users.noreply.github.com>
2025-06-06 00:01:48 -0500

    Add EstudianteSeeder

--- CODIGO PROYECTO/BACKEND/database/seeders/DatabaseSeeder.php
+++ CODIGO PROYECTO/BACKEND/database/seeders/DatabaseSeeder.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        // Llama a tu seeder personalizado
+        
         $this->call([
             AcudienteSeeder::class,
+            EstudianteSeeder::class,
+            
         ]);
     }
 }
