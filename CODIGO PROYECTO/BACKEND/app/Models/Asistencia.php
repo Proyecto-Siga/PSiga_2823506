e72e5c0ffed7e2becea6d6450112ff589eacb893
EstibenCG <183879300+EstibenCG@users.noreply.github.com>
2025-07-04 00:40:23 -0500

    Add anotaciones Swagger y relaciones en modelo Asistencia

--- CODIGO PROYECTO/BACKEND/app/Models/Asistencia.php
+++ CODIGO PROYECTO/BACKEND/app/Models/Asistencia.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Asistencia",
+ *     title="Asistencia",
+ *     description="Esquema del modelo Asistencia",
+ *     required={"clase_id", "estudiante_id", "estado"},
+ *     @OA\Property(property="id", type="integer", example=10),
+ *     @OA\Property(property="clase_id", type="integer", example=1),
+ *     @OA\Property(property="estudiante_id", type="integer", example=5),
+ *     @OA\Property(property="estado", type="integer", example=1, description="0: falta, 1: presente")
+ * )
+ */
+
+
 class Asistencia extends Model
 {
     use HasFactory;
