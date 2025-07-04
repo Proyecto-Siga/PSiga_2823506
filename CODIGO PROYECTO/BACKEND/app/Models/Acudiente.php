2cc48e06ad6b062a8238f3adc700975ce819a4bf
EstibenCG <183879300+EstibenCG@users.noreply.github.com>
2025-07-04 00:40:17 -0500

    Add anotaciones Swagger y relaciones en modelo Acudiente

--- CODIGO PROYECTO/BACKEND/app/Models/Acudiente.php
+++ CODIGO PROYECTO/BACKEND/app/Models/Acudiente.php
@@ -5,6 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Acudiente",
+ *     title="Acudiente",
+ *     description="Modelo que representa un acudiente",
+ *     required={"nombre", "apellido", "correo"},
+ *     @OA\Property(property="nombre", type="string", example="Lucía"),
+ *     @OA\Property(property="apellido", type="string", example="Ramírez"),
+ *     @OA\Property(property="correo", type="string", format="email", example="lucia.ramirez@example.com")
+ * )
+ */
+
+
 class Acudiente extends Model
 {
     use HasFactory;
