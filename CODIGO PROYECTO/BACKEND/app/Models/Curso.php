7a5a0c61e8a9584e7b43a89c11f826e749c1a959
EstibenCG <183879300+EstibenCG@users.noreply.github.com>
2025-07-04 00:40:28 -0500

    Add anotaciones Swagger y relaciones en modelo Curso

--- CODIGO PROYECTO/BACKEND/app/Models/Curso.php
+++ CODIGO PROYECTO/BACKEND/app/Models/Curso.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Curso",
+ *     title="Curso",
+ *     description="Esquema del modelo Curso",
+ *     required={"nombre"},
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="nombre", type="string", example="Octavo A")
+ * )
+ */
+
+
 class Curso extends Model
 {
     use HasFactory;
