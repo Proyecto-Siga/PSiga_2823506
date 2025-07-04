eccb84c8a65fd46a2d541f64fe8f295dd06e1233
EstibenCG <183879300+EstibenCG@users.noreply.github.com>
2025-07-04 00:40:25 -0500

    Add anotaciones Swagger y relaciones en modelo Aula

--- CODIGO PROYECTO/BACKEND/app/Models/Aula.php
+++ CODIGO PROYECTO/BACKEND/app/Models/Aula.php
@@ -4,6 +4,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Aula",
+ *     title="Aula",
+ *     description="Modelo de Aula",
+ *     required={"nombre_aula"},
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="nombre_aula", type="string", example="Aula 101"),
+ *     
+ * )
+ */
+
+
 class Aula extends Model
 {
     use HasFactory;
